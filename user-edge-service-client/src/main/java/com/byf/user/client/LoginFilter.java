16a1dce32d16511593dedf95cdc7a103fa60de77
byf312358196 <baiyifan@163.com>
2019-09-25 23:52:53 +0800

    project(DevOps):添加微服务网关Zuul
    Signed-off-by: byf312358196 <baiyifan@163.com>

--- user-edge-service-client/src/main/java/com/byf/user/client/LoginFilter.java
+++ user-edge-service-client/src/main/java/com/byf/user/client/LoginFilter.java
@@ -64,7 +64,7 @@ public abstract class LoginFilter implements Filter {
         }
 
         if(userDTO==null) {
-            response.sendRedirect("http://127.0.0.1:8082/user/login");
+            response.sendRedirect("http://127.0.0.1:8080/user/login");
             return;
         }
 
