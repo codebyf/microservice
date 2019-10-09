e459a2ae1e8db605b6b7bdead7ee991f485d957b
byf312358196 <baiyifan@163.com>
2019-10-09 22:58:24 +0800

    project(DockerSwarm)：配置service.yml，部署集群服务
    Signed-off-by: byf312358196 <baiyifan@163.com>

--- user-edge-service-client/src/main/java/com/byf/user/client/LoginFilter.java
+++ user-edge-service-client/src/main/java/com/byf/user/client/LoginFilter.java
@@ -64,7 +64,7 @@ public abstract class LoginFilter implements Filter {
         }
 
         if(userDTO==null) {
-            response.sendRedirect("http://192.168.1.101/user/login");
+            response.sendRedirect("http://www.micro-service.com/user/login");
             return;
         }
 
